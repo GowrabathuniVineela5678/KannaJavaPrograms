1699014273a3825ba9c85d7e3baec25a06d1e892
Vineela <gowrabathunivineela@gmail.com>
2025-07-24 15:32:02 +0530

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        char[] chars = original.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        System.out.println("Reversed: " + new String(chars));
+        sc.close();
+    }
+}
